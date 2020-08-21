0a0edf8f48f3502e6c2177e61dbe72df504ae45d
Size <55809336+Size00@users.noreply.github.com>
2020-08-20 20:09:37 -0500

    Update channelDS.php
    made it more usable

--- channelDS.php
+++ channelDS.php
@@ -1,16 +1,22 @@
 <script>
 <?php
-function ch_scrape($ch_id){
+function youtubescrapper($ch_id){
     libxml_use_internal_errors(true);
     $doc = new DOMDocument();
     $doc->loadHTMLFile("http://www.youtube.com/channel/$ch_id/live");
     foreach($doc->getElementsByTagName('script') as $table){     
-	   echo $tables = $table->lastChild->textContent; 
+	   echo $tables = $table->lastChild->textContent;
 	}
 }
- ch_scrape('UCSJ4gkVC6NrvII8umztf0Ow');	 
+ youtubescrapper('UCSJ4gkVC6NrvII8umztf0Ow');	 
 ?>
 
-console.log(window["ytInitialData"]);
+//['currentVideoEndpoint']['watchEndpoint']['videoId'] // video id
+//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['title']['runs'][0]['text'] // video title
+//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['viewCount']['videoViewCountRenderer']['viewCount']['simpleText'] // view count
+//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['dateText']['simpleText']// date published
 
+yt_data = window["ytInitialData"].contents.twoColumnWatchNextResults;
+
+console.log(yt_data);
 </script>
