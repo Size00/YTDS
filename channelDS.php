85f6f110c9579abce1ed6443dde74eefd568054b
Size <55809336+Size00@users.noreply.github.com>
2020-08-22 04:31:36 -0500

    Update channelDS.php

--- channelDS.php
+++ channelDS.php
@@ -10,12 +10,7 @@ function youtubescrapper($ch_id){
 }
  youtubescrapper('UCSJ4gkVC6NrvII8umztf0Ow');	 
 ?>
-
-//['currentVideoEndpoint']['watchEndpoint']['videoId'] // video id
-//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['title']['runs'][0]['text'] // video title
-//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['viewCount']['videoViewCountRenderer']['viewCount']['simpleText'] // view count
-//['contents']['twoColumnWatchNextResults']['results']['results']['contents'][0]['videoPrimaryInfoRenderer']['dateText']['simpleText']// date published
-
+	
 yt_data_viewCount = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.runs[0].text;
 yt_data_title = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
 yt_latest_title = yt_data_title.replace(/[\u{0080}-\u{FFFF}]/gu,"");
