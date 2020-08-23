8a0963a88eb2e030145f61b3e2f91e2e937e62ed
Size <55809336+Size00@users.noreply.github.com>
2020-08-23 10:25:16 -0500

    Update channelDS.php

--- channelDS.php
+++ channelDS.php
@@ -1,14 +1,13 @@
 <script>
 <?php
-  $ytid = $user->youtubeid;
+  function yt_data($ch_id){
   $doc = new DOMDocument();
-  $doc->loadHTMLFile("http://www.youtube.com/channel/UCv9Edl_WbtbPeURPtFDo-uA/live");
+  $doc->loadHTMLFile("http://www.youtube.com/channel/$ch_id/live");
   $doc_s = $doc->getElementsByTagName('script');	
   $tables = array();
-  foreach($doc_s as $table){     
-     $tables[] = $table->nodeValue;
-   }
-  echo $tables[31];
+  foreach($doc_s as $table){$tables[] = $table->nodeValue;}
+  echo $tables[31]; }
+  yt_data("")// youtube channel id 
 ?>	
 yt_data_viewCount = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.runs[0].text;
 yt_data_title = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
