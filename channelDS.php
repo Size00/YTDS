04f1c3117aa0a211ab86e3983f67e8ee468757bf
Size <55809336+Size00@users.noreply.github.com>
2020-08-23 09:28:26 -0500

    Update channelDS.php

--- channelDS.php
+++ channelDS.php
@@ -1,24 +1,25 @@
 <script>
 <?php
-function youtubescrapper($ch_id){
-    libxml_use_internal_errors(true);
-    $doc = new DOMDocument();
-    $doc->loadHTMLFile("http://www.youtube.com/channel/$ch_id/live");
-    foreach($doc->getElementsByTagName('script') as $table){     
-	   echo $table->lastChild->textContent;
-	}
-}
- youtubescrapper('UCSJ4gkVC6NrvII8umztf0Ow');	 
-?>
-	
+  $ytid = $user->youtubeid;
+  $doc = new DOMDocument();
+  $doc->loadHTMLFile("http://www.youtube.com/channel/UCv9Edl_WbtbPeURPtFDo-uA/live");
+  $doc_s = $doc->getElementsByTagName('script');	
+  $tables = array();
+  foreach($doc_s as $table){     
+     $tables[] = $table->nodeValue;
+   }
+  echo $tables[31];
+?>	
 yt_data_viewCount = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.runs[0].text;
 yt_data_title = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
 yt_latest_title = yt_data_title.replace(/[\u{0080}-\u{FFFF}]/gu,"");
-	
+
 $(document).ready(function(){
-    $("ytdata").html(yt_data_viewCount);
+    $("ytdata").html(yt_data_title);
 });
-	
-	
+//alert(yt_data_viewCount + " \n"  +  yt_latest_title);
+
+console.log(window["ytInitialData"]); 
+
 </script>
 <ytdata><ytdata> <!-- prints data-->
