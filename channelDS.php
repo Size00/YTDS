e5d1627fb6eae6f144211ae227396fba531ac2b5
Size <55809336+Size00@users.noreply.github.com>
2020-08-23 10:26:22 -0500

    Update channelDS.php

--- channelDS.php
+++ channelDS.php
@@ -10,15 +10,11 @@
   yt_data("")// youtube channel id 
 ?>	
 yt_data_viewCount = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.viewCount.videoViewCountRenderer.viewCount.runs[0].text;
-yt_data_title = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text;
-yt_latest_title = yt_data_title.replace(/[\u{0080}-\u{FFFF}]/gu,"");
-
+yt_data_title = window["ytInitialData"].contents.twoColumnWatchNextResults.results.results.contents[0].videoPrimaryInfoRenderer.title.runs[0].text.replace(/[\u{0080}-\u{FFFF}]/gu,"");
 $(document).ready(function(){
     $("ytdata").html(yt_data_title);
 });
-//alert(yt_data_viewCount + " \n"  +  yt_latest_title);
-
+//alert(yt_data_viewCount + " \n"  +  yt_data_title);
 console.log(window["ytInitialData"]); 
-
 </script>
 <ytdata><ytdata> <!-- prints data-->
